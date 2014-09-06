ebc206b0dba8d0d9c65d144318be957136d5d1fc
rcadaoas <romeo.mail7@gmail.com>
2014-09-06 21:13:58 +0400

    placesexplorer controller and placesresults page

--- FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
@@ -1,4 +1,7 @@
 ﻿//$scope is the glue between the controller and the view
+//we can think of it as the container holding the data that we want 
+//to project on the view
 app.controller('placesExplorerController', function ($scope) {
+    //in this line we added a new model "exploreNearby" in the $scope object
     $scope.exploreNearby = "New York";
 });
