0facb228c47c3bf855ee99fa55b952be228e2a13
rcadaoas <romeo.mail7@gmail.com>
2014-09-07 22:02:04 +0400

    places explorercontroller

--- FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
@@ -1,7 +1,74 @@
 ﻿//$scope is the glue between the controller and the view
 //we can think of it as the container holding the data that we want 
 //to project on the view
-app.controller('placesExplorerController', function ($scope) {
+app.controller('placesExplorerController', function ($scope,placesExplorerService,$filter) {
     //in this line we added a new model "exploreNearby" in the $scope object
     $scope.exploreNearby = "New York";
+    $scope.exploreQuery = "";
+    $scope.filterValue = "";
+
+    $scope.places = [];
+    $scope.filteredPlaces = [];
+    $scope.filteredPlacesCount = 0;
+
+    //paging
+    $scope.totalRecordsCount = 0;
+    $scope.pageSize = 10;
+    $scope.currentPage = 1;
+
+    init();
+
+    function init() {
+        createWatche();
+        getPlaces();
+    }
+
+    function getPlaces() {
+
+        var offset = ($scope.pageSize) * ($scope.currentPage - 1);
+
+        //we were able to issue HTTP GET request to the factory we created.
+        //arguments is pass by (key, value) pairs. (i.e. near,query, limit)
+        placesExplorerService.get({ near: $scope.exploreNearby,query: $scope.exploreQuery, limit: $scope.pageSize, offset: offset },function(placesResult){
+
+            if (placesResult.response.groups) {
+                $scope.places = placesResult.response.groups[0].items;
+                $scope.totalRecordsCount = placesResult.response.totalResults;
+                filterPlaces('');
+            }
+            else {
+                $scope.places = [];
+                $scope.totalRecordsCount = 0;
+            }
+        });
+    };
+
+    function createWatche() {
+        //added listener for the filterValue attribute
+        //this listener gets fired when the filterValue has changed.
+        $scope.$watch("filterValue", function (filterInput) {
+            filterPlaces(filterInput);
+        });
+    }
+
+    //will be called on the explore button clicked
+    $scope.doSearch = function () {
+        $scope.currentPage = 1;
+        getPlaces();
+    };
+
+    $scope.pageChanged = function (page) {
+        $scope.currentPage = page;
+        getPlaces();
+    };
+
+    $scope.buildCategoryIcon = function (icon) {
+        return icon.prefix + '44' + icon.suffix;
+    };
+
+    $scope.buildVenueThumbnail = function (photo) {
+        return photo.items[0].prefix + '128x128' + photo.items[0].suffix;
+    };
+
+
 });
---
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/services/placesExplorerService.js
@@ -0,0 +1,48 @@
+﻿/*ways to create Angular Service
+1. Service
+Syntax: module.service( 'serviceName', function );
+Result: When declaring serviceName as an injectable argument you will be provided with an instance of the function. In other words new FunctionYouPassedToService().
+2. Factory
+Syntax: module.factory( 'factoryName', function );
+Result: When declaring factoryName as an injectable argument you will be provided with the value that is returned by invoking the function reference passed to module.factory.
+3. Provider
+Syntax: module.provider( 'providerName', function );
+Result: When declaring providerName as an injectable argument you will be provided with ProviderFunction().$get(). The constructor function is instantiated before the $get method is called - ProviderFunction is the function reference passed to module.provider.
+
+ref:http://stackoverflow.com/questions/15666048/service-vs-provider-vs-factory?lq=1
+*/
+
+/*This service is repsonsible for sending 
+HTTP request to Foursquare explore places API*/
+
+/*For more info about Foursquare API and how to get a client ID and secret visit
+https://developer.foursquare.com/docs/venues/explore*/
+
+var requestParms = {
+    clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
+    clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
+    version: "20131230"
+}
+
+//$resource built in Angular service which allow us to interact with RESTful data sources.
+//we can also use lower level built-in service $http
+app.factory('placesExplorerService', function ($resource) {
+
+    //:action param will be translated to a part of the request URL and any other 
+    //params will be treated as query string (key,value) pairs.
+    var requestUri = 'https://api.foursquare.com/v2/venues/:action';
+
+    //$resource configuration:
+    return $resource(requestUri,
+        {
+            action: 'explore',
+            client_id: requestParms.clientId,
+            client_secret: requestParms.clientSecret,
+            v: requestParms.version,
+            venuePhotos: '1',
+            callback: 'JSON_CALLBACK'
+        },
+        {
+            get: { method: 'JSONP' }
+        });
+});
