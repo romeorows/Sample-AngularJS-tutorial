7dfd24751869139a773624f963c2abfe7045e5b4
rcadaoas <romeo.mail7@gmail.com>
2014-09-07 22:10:29 +0400

    placeNameCategoryFilter

---
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/filters/placeNameCategoryFilter.js
@@ -0,0 +1,23 @@
+﻿app.filter("placeNameCategoryFilter", function () {
+    return function (places,filterValue) {
+        if (!filterValue) return places;
+
+        var matches = [];
+
+        filterValue = filterValue.toLowerCase();
+        
+        for (var i = 0; i < places.length; i++) {
+            var place = places[i];
+
+            if(place.venue.name.toLowerCase().indexOf(filterValue) > -1 ||
+                places.venue.categories[0].shortName.toLowerCase.indexOf(filterValue) > -1)
+            {
+                matches.push(place);
+            }
+        }
+
+        return matches;
+
+    };
+
+});
