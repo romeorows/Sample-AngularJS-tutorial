4c1c3f5ccc3c0308cbe83311addae0fc16ff480e
rcadaoas <romeo.mail7@gmail.com>
2014-09-12 16:31:47 +0400

    $modal not #model fuck.....

--- FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/controllers/placesExplorerController.js
@@ -1,7 +1,7 @@
 ﻿//$scope is the glue between the controller and the view
 //we can think of it as the container holding the data that we want 
 //to project on the view
-app.controller('placesExplorerController', function ($scope, placesExplorerService, placesPhotosService, $filter, $model) {
+app.controller('placesExplorerController', function ($scope, placesExplorerService, placesPhotosService, $filter, $modal) {
     //in this line we added a new model "exploreNearby" in the $scope object
     $scope.exploreNearby = "New York";
     $scope.exploreQuery = "";
