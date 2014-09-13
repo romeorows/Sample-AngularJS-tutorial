4e2b63766d56126043ec7718d067d35c30557903
rcadaoas <romeo.mail7@gmail.com>
2014-09-13 15:46:11 +0400

    placesdataservice

---
+++ FoursquareAngularJS/FoursquareAngularJS.Web/app/services/PlacesDataService.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+app.factory('placesDataService', function ($http, toaster) {
+
+    var serviceBase = '/api/places/';
+    var placesDataFactory = {};
+    var userInContext = null;
+
+    var _getUserInCtx = function (userInCtx) {
+        userInContext = userInCtx;
+    };
+
+    var _savePlace = function (venue) {
+    }
+
+});
