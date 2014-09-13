30a76174fb2c658dfd95b1f18737b6046b0fe514
rcadaoas <romeo.mail7@gmail.com>
2014-09-13 15:39:47 +0400

    API Controllers

--- FoursquareAngularJS/FoursquareAngularJS.Web/App_Start/WebApiConfig.cs
+++ FoursquareAngularJS/FoursquareAngularJS.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace FoursquareAngularJS.Web
@@ -9,16 +11,21 @@ namespace FoursquareAngularJS.Web
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                name: "PlacesRoute",
+                routeTemplate: "api/places/{userName}",
+                defaults: new { controller= "places", userName = RouteParameter.Optional}
+                );
+
+            config.Routes.MapHttpRoute(
+                name:"UsersRoute",
+                routeTemplate: "/api/users/{userName}",
+                defaults: new {controller="users"}
+                );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ FoursquareAngularJS/FoursquareAngularJS.Web/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+﻿using FoursquareAngularJS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FoursquareAngularJS.Web.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        private IFourSquareRepository _repo;
+
+        protected IFourSquareRepository TheRepository
+        {
+            get
+            {
+                _repo = new FourSquareRepository();
+                return _repo;
+            }
+        }
+    }
+}
